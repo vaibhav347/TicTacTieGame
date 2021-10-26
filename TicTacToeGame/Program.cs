4699adfe4006bbeb7076d466f2152638e5f77611
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-26 18:34:20 +0530

    ok

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacTieGamme obj = new TicTacTieGamme();
+            obj.createBoard();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacTieGamme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    public class TicTacTieGamme
+    {
+        public void createBoard()
+        {
+            char[] board = new char[11];
+            for (int i = 1; i <= 10; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
